bae83ae18e64d9f060691a69f4171629748b2d64
josemaria-codes101 <josemaria.ofor@pau.edu.ng>
2025-12-01 22:15:03 +0100

    week 10 practices

---
+++ week-10/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\j.oforCOS101\week-10\practice_5\target\debug\practice_5.exe: C:\Users\user\Documents\j.oforCOS101\week-10\practice_5\src\main.rs
---
+++ week-10/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\j.oforCOS101\week-10\practice_6\target\debug\practice_6.exe: C:\Users\user\Documents\j.oforCOS101\week-10\practice_6\src\main.rs
