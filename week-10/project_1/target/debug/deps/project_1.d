efa906f582f444d6c0c47d17d4e73ab880c2a9d7
josemaria-codes101 <josemaria.ofor@pau.edu.ng>
2025-12-07 23:57:02 +0100

    project 1

---
+++ week-10/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\j.oforCOS101\week-10\project_1\target\debug\deps\project_1.d: src\main.rs
+
+C:\Users\user\Documents\j.oforCOS101\week-10\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\j.oforCOS101\week-10\project_1\target\debug\project_1.exe: C:\Users\user\Documents\j.oforCOS101\week-10\project_1\src\main.rs
